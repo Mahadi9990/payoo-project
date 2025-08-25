77c0a531d746ccff97b3cd7cdeb37d9b6f4e4bb1
Mahadi9990 <mdshafayat9999@gmail.com>
2025-08-25 22:29:51 +0600

    26.5 cm

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 document.getElementById('login').addEventListener('click',(e)=>{
     e.preventDefault()
-    alert('login successfully')
-    console.log('login successfully')
+    const mobileNumberDataBase = 8801758413983;
+    const pinNumberDataBase = 1234
+    const mobileNumber = document.getElementById('mobileNumber').value
+    const pinNumber = document.getElementById('pinNumber').value
+    const convertedMobileNumber = parseInt(mobileNumber)
+    const convertedPinNumber = parseInt(pinNumber)
+
+    if( mobileNumberDataBase === convertedMobileNumber && pinNumberDataBase === convertedPinNumber){
+        window.location.href = './home.html'
+    }else{
+        alert("wrong cradintails")
+    }
 })
