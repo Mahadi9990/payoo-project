bbb8eaee0a09a53020696fcf78c568edf746b3d4
Mahadi9990 <mdshafayat9999@gmail.com>
2025-08-29 09:16:01 +0600

    cm 26 all class

--- home.js
+++ home.js
@@ -1,7 +1,6 @@
 document.getElementById("add_money").addEventListener("click",function(e){
     e.preventDefault()
     const validPin = 1234;
-    const validNumber = 10000000000
     const bank  = document.getElementById("bank").value
     const account = document.getElementById("bank_account_number").value
     const addMoney = parseInt(document.getElementById("add_amount").value)
@@ -19,4 +18,34 @@ document.getElementById("add_money").addEventListener("click",function(e){
 
     const totalBalance = addMoney + avableBalance
     document.getElementById("avableBalance").innerText = totalBalance
+})
+document.getElementById("cashout_add_money").addEventListener("click",function(e){
+    e.preventDefault()
+    const validPin = 1234;
+    const account = document.getElementById("cashout_bank_account_number").value
+    const addMoney = parseInt(document.getElementById("cashout_add_amount").value)
+    const pinNumber = parseInt(document.getElementById("cashout_pin_number").value)
+    const avableBalance = parseInt(document.getElementById("avableBalance").innerText)
+
+    if( account.length < 11){
+        alert("Provider a valid account number")
+        return
+    }
+    if( pinNumber !== validPin){
+        alert("Provider a currect pin number")
+        return
+    }
+
+    const totalBalance = avableBalance - addMoney
+    document.getElementById("avableBalance").innerText = totalBalance
+})
+
+
+document.getElementById("add_money_button").addEventListener("click",()=>{
+    document.getElementById("add_money_parents").style.display = "block"
+    document.getElementById("chachOut_parents").style.display = "none"
+})
+document.getElementById("cashOut_button").addEventListener("click",()=>{
+    document.getElementById("add_money_parents").style.display = "none"
+    document.getElementById("chachOut_parents").style.display = "block"
 })
